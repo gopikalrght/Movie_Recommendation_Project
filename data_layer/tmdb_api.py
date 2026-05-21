b2430bb2c88ae4591bb3c3ebdbc079bcdb037d01
Your Name <your_email@gmail.com>
2026-05-21 19:43:33 +0530

    Added reusable OMDB API search function

--- data_layer/tmdb_api.py
+++ data_layer/tmdb_api.py
@@ -1,58 +1,21 @@
 import requests
 
-API_KEY = "1204abf9d5acbc28450fc50bc1566b43"
+API_KEY = "78cdf749"
 
 
-fallback_movies = [
-    {
-        "movie_id": 1,
-        "title": "Avatar",
-        "rating": 8.2,
-        "genre": "Sci-Fi"
-    },
-    {
-        "movie_id": 2,
-        "title": "Interstellar",
-        "rating": 8.6,
-        "genre": "Sci-Fi"
-    },
-    {
-        "movie_id": 3,
-        "title": "Titanic",
-        "rating": 7.9,
-        "genre": "Romance"
+def search_movie(movie_name):
+
+    url = f"http://www.omdbapi.com/?apikey={API_KEY}&t={movie_name}"
+
+    response = requests.get(url)
+
+    data = response.json()
+
+    movie = {
+        "title": data.get("Title"),
+        "year": data.get("Year"),
+        "genre": data.get("Genre"),
+        "rating": data.get("imdbRating"),
+        "poster":data.get("Poster")
     }
-]
-
-
-def get_popular_movies():
-
-    url = f"https://api.themoviedb.org/3/movie/popular?api_key={API_KEY}"
-
-    try:
-
-        response = requests.get(url, timeout=10)
-
-        data = response.json()
-
-        movies = []
-
-        for movie in data["results"]:
-
-            movies.append({
-                "title": movie["title"],
-                "rating": movie["vote_average"]
-            })
-
-        print("Live TMDB data loaded")
-
-        return movies
-
-    except:
-
-        print("TMDB failed — using fallback local data")
-
-        return fallback_movies
-
-
-print(get_popular_movies())
+    return movie
